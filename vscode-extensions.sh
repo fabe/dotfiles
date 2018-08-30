4cb3c68e81fd4246bf0b9cc91a940329c059171b
Fabian Schultz <fabian.schultz@stinkstudios.com>
2018-08-30 10:45:15 -0400

    Add vscode extensions.

---
+++ vscode-extensions.sh
@@ -0,0 +1,15 @@
+# Base extensions (not including some language-specific ones)
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscodecode --install-extension formulahendry.auto-rename-tag
+code --install-extension joshpeng.sublime-babel-vscodecode --install-extension jpoissonnier.vscode-styled-components
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension naumovs.color-highlightcode --install-extension octref.vetur
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension sdras.night-owl
+code --install-extension shyykoserhiy.vscode-spotify
+code --install-extension silvenon.mdx
+code --install-extension Tyriar.sort-lines
+code --install-extension vilicvane.es-quotes
+code --install-extension wix.vscode-import-cost
